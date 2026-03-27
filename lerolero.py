02ff39804d9eb4d9678d5f145be7c814db1c7474
Murilo Gueler <murilogueler@usp.br>
2026-03-27 11:31:00 -0300

    mudei codigo

--- lerolero.py
+++ lerolero.py
@@ -1,3 +1,14 @@
 """ Gerador de lerolero
 
 Gera frases de efeito sem sentido real."""
+import random
+
+#cada frase é composta por 3 partes aleatorias
+#lista de possibilidades para cada uma das partes
+parte1 = []
+parte2 = []
+parte3 = []
+
+pint (random.choice(parte1),random.choice(parte2),random.choice(parte3))
+
+
